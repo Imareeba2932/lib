56b0a893ee6eb1ab966959e57b83552c6581cbc6
Areeba Shakeel <shakeelareeba02>
2023-10-02 11:36:47 +0530

    libraries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+import Nav from './components/nav';
+import Log from './components/log';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      {/* <Log /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="log" element={<Log />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/login.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import Swal from 'sweetalert2'
+import {motion} from "framer-motion"
+import { useState } from 'react';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleLogin = () => {
+      // You can add your authentication logic here.
+      // For this example, let's assume a successful login if both fields are filled.
+      if (username !== '' && password !== '') {
+        Swal.fire({
+          icon: 'success',
+          title: 'Login Successful!',
+          text: 'Welcome back, ' + username + '!',
+        });
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Login Failed!',
+          text: 'Please fill in both username and password fields.',
+        });
+      }
+    };
+  
+    return (
+        <motion.div
+            initial={{x: '100%'}}
+            animate={{x: 0}}
+            exit={{x: '-100%'}}
+            transition={{duration: 0.3}}
+            className='container'>
+
+            <dic className="col">
+                <div className="card mt-5 d-block mx-auto c">
+                    <div className="card-header text-center">LOGIN</div>
+                    <div className="card-body">
+                        <form>
+                            <div className="form-group">
+                                <label htmlFor="username">Username</label>
+                                <input type="text"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                 className="form-control mb-4"/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <input type="text"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                 className="form-control mb-4"/>
+                            </div>
+                            <button onClick={handleLogin} className="btn btn-primary">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </dic>
+        </motion.div>
+    );
+  }
+  
+  export default Login;
+  
